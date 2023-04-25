477c5114b68800eb6b560c7e9db05e1c7eed0d05
William Clark <Austin.farback9@gmail.com>
2023-04-25 16:39:01 -0700

    [Will Branch] - Started water and waterfall

---
+++ Assets/GameAssets/Shaders/WaterParticles/SetInteractiveShaderEffects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetInteractiveShaderEffects : MonoBehaviour
+{
+    [SerializeField] private RenderTexture _rt = null;
+    [SerializeField] private Transform _target = null;
+    [SerializeField] private Camera _camera = null;
+
+    void Awake()
+    {
+        Shader.SetGlobalTexture("_GlobalEffectRT", _rt);
+        Shader.SetGlobalFloat("_OrthographicCamSize", _camera.orthographicSize);
+    }
+
+    private void Update()
+    {
+        //transform.position = new Vector3(_target.transform.position.x, transform.position.y, _target.transform.position.z);
+        transform.position = new Vector3(_target.transform.position.x, transform.position.y, _target.transform.position.z);
+        Shader.SetGlobalVector("_Position", transform.position);
+    }
+
+
+}
