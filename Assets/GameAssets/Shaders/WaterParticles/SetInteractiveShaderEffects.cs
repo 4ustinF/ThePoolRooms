06919db645f286fccb7af050a1b6c96b3fe439de
William Clark <Austin.farback9@gmail.com>
2023-04-25 18:17:46 -0700

    [Will Branch] - Got water interactions working

--- Assets/GameAssets/Shaders/WaterParticles/SetInteractiveShaderEffects.cs
+++ Assets/GameAssets/Shaders/WaterParticles/SetInteractiveShaderEffects.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SetInteractiveShaderEffects : MonoBehaviour
 {
@@ -8,7 +6,7 @@ public class SetInteractiveShaderEffects : MonoBehaviour
     [SerializeField] private Transform _target = null;
     [SerializeField] private Camera _camera = null;
 
-    void Awake()
+    private void Awake()
     {
         Shader.SetGlobalTexture("_GlobalEffectRT", _rt);
         Shader.SetGlobalFloat("_OrthographicCamSize", _camera.orthographicSize);
@@ -16,10 +14,7 @@ public class SetInteractiveShaderEffects : MonoBehaviour
 
     private void Update()
     {
-        //transform.position = new Vector3(_target.transform.position.x, transform.position.y, _target.transform.position.z);
         transform.position = new Vector3(_target.transform.position.x, transform.position.y, _target.transform.position.z);
         Shader.SetGlobalVector("_Position", transform.position);
     }
-
-
 }
