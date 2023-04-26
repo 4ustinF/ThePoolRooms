752cf63a2a500021bc7d8c93317315ab3ef72d54
William Clark <Austin.farback9@gmail.com>
2023-04-26 16:06:28 -0700

    [Will Branch] - Added waterFall Manager

---
+++ Assets/Scripts/Editor/WaterFallEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+[CustomEditor(typeof(WaterFallManager))]
+public class WaterFallEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var script = (WaterFallManager)target;
+
+        if (GUILayout.Button("Start WaterFall", GUILayout.Height(30)))
+        {
+            script.StartWaterFall();
+        }
+
+        if (GUILayout.Button("Stop WaterFall", GUILayout.Height(30)))
+        {
+            script.StopWaterFall();
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/WaterFallManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaterFallManager : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _waterFallPS = null;
+    [SerializeField] private ParticleSystem _dustFallPS = null;
+    [SerializeField] private ParticleSystem _wavesPS = null;
+    [SerializeField] private ParticleSystem _sprinklesPS = null;
+    [SerializeField] private ParticleSystem _surfaceDust1PS = null;
+    [SerializeField] private ParticleSystem _surfaceDust2PS = null;
+    [SerializeField] private float _waitTime = 0.1f;
+
+    public void StartWaterFall()
+    {
+        _waterFallPS.Play();
+        _dustFallPS.Play();
+        StartCoroutine(StartWaterDebris());
+    }
+
+    private IEnumerator StartWaterDebris()
+    {
+        yield return new WaitForSeconds(_waitTime);
+        _wavesPS.Play();
+        _sprinklesPS.Play();
+        _surfaceDust1PS.Play();
+        _surfaceDust2PS.Play();
+    }
+
+    public void StopWaterFall()
+    {
+        // Kill StartWaterDebris routine if its active
+
+        _waterFallPS.Stop();
+        _dustFallPS.Stop();
+        _wavesPS.Stop();
+        _sprinklesPS.Stop();
+        _surfaceDust1PS.Stop();
+        _surfaceDust2PS.Stop();
+    }
+}
