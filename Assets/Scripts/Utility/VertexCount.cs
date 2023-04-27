c14324a2f5f60eb39bb57066905db3b56e2a1129
William Clark <Austin.farback9@gmail.com>
2023-04-27 04:12:33 -0700

    [Will Branch] - Updated Vertex Tool

--- Assets/Scripts/Utility/VertexCount.cs
+++ Assets/Scripts/Utility/VertexCount.cs
@@ -5,16 +5,35 @@ public class VertexCount : MonoBehaviour
 {
     public void LogVertexCount()
     {
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
         if (mesh != null)
         {
-            Debug.Log($"Mesh Count: {mesh.vertexCount}");
+            Debug.Log($"MeshFilter Vertex Count: {mesh.vertexCount}");
         }
 
         ProBuilderMesh proBuilderMesh = GetComponent<ProBuilderMesh>();
         if (proBuilderMesh != null)
         {
-            Debug.Log($"Probuilder Mesh Count: {proBuilderMesh.vertexCount}");
+            Debug.Log($"ProBuilderMesh Vertex Count: {proBuilderMesh.vertexCount}");
         }
+    }
+
+    public void LogTotalVertexCount()
+    {
+        int count = 0;
+
+        var meshFilters = GameObject.FindObjectsOfType<MeshFilter>();
+        foreach (var meshFilter in meshFilters)
+        {
+            count += meshFilter.sharedMesh.vertexCount;
+        }
+
+        var proBuilderMeshs = GameObject.FindObjectsOfType<ProBuilderMesh>();
+        foreach (var mesh in proBuilderMeshs)
+        {
+            count += mesh.vertexCount;
+        }
+
+        Debug.Log($"Vertex Count: {count}");
     }
 }
