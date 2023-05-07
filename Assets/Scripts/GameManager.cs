200096a6d5a7d8d31bc03f01f5947c0ccafb078d
William Clark <Austin.farback9@gmail.com>
2023-05-06 23:23:34 -0700

    [Will Branch] - Continued working on animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,156 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    //[SerializeField] private AudioManager _audioManager = null;
+    private enum CurrentEvent
+    {
+        None = -1,
+        Event1,
+        Event2,
+        Event3,
+        Event4,
+        Event5,
+        Event6,
+        Event7,
+        Count
+    }
+
+    private static GameManager _instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+
+    [Header("References")]
     //[SerializeField] private WaterFallManager _waterFallManager = null;
+    [SerializeField] private AudioSource _audioSource = null;
+
+    //[Header("Ambience")]
+    //[SerializeField] private AudioClip _backgroundMusic = null;
+    //[SerializeField] private AudioClip _waterAmbience = null;
+
+    //[Header("SFX")]
+    //[SerializeField] private AudioClip _waterSplashSFX = null;
+
+    [Header("DialougeAudioClips")]
+    [SerializeField] private AudioClip _dialougeClip1 = null;
+    [SerializeField] private AudioClip _dialougeClip2 = null;
+    [SerializeField] private AudioClip _dialougeClip3 = null;
+    [SerializeField] private AudioClip _dialougeClip4 = null;
+    [SerializeField] private AudioClip _dialougeClip5 = null;
+    [SerializeField] private AudioClip _dialougeClip6 = null;
+    [SerializeField] private AudioClip _dialougeClip7 = null;
+
+    [SerializeField] private CurrentEvent _currentEvent = CurrentEvent.None;
+
+    private void Start()
+    {
+        //MoveToNextEvent();
+    }
+
+    private void MoveToNextEvent()
+    {
+        int newEvent = (int)_currentEvent + 1;
+        int maxEvent = (int)CurrentEvent.Count;
+        _currentEvent = (CurrentEvent)Mathf.Min(newEvent, maxEvent);
+        PlayAudio();
+    }
+
+    private void PlayAudio()
+    {
+        switch (_currentEvent)
+        {
+            case CurrentEvent.Event1:
+                PlayEvent(_dialougeClip1, _dialougeClip1.length + 1.0f);
+                break;
+            case CurrentEvent.Event2:
+                PlayEvent(_dialougeClip2, _dialougeClip2.length + 1.0f);
+                break;
+            case CurrentEvent.Event3:
+                PlayEvent(_dialougeClip3, _dialougeClip3.length + 1.0f);
+                break;
+            case CurrentEvent.Event4:
+                PlayEvent(_dialougeClip4, _dialougeClip4.length + 1.0f);
+                break;
+            case CurrentEvent.Event5:
+                PlayEvent(_dialougeClip5, _dialougeClip5.length + 1.0f);
+                break;
+            case CurrentEvent.Event6:
+                PlayEvent(_dialougeClip6, _dialougeClip6.length + 1.0f);
+                break;
+            case CurrentEvent.Event7:
+                PlayEvent(_dialougeClip7, _dialougeClip7.length + 1.0f);
+                break;
+        }
+    }
+
+    private void PlayEvent(AudioClip clip, float eventWaitTime)
+    {
+        _audioSource.PlayOneShot(clip);
+        StartCoroutine(PlayEventAnimationRoutine(eventWaitTime));
+    }
+
+    private IEnumerator PlayEventAnimationRoutine(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        PlayEventAnimation();
+    }
+
+    private void PlayEventAnimation()
+    {
+        switch (_currentEvent)
+        {
+            case CurrentEvent.Event1:
+                break;
+            case CurrentEvent.Event2:
+                break;
+            case CurrentEvent.Event3:
+                break;
+            case CurrentEvent.Event4:
+                break;
+            case CurrentEvent.Event5:
+                break;
+            case CurrentEvent.Event6:
+                break;
+            case CurrentEvent.Event7:
+                break;
+        }
+
+        Debug.Log($"Played event: {_currentEvent}");
+        MoveToNextEvent();
+    }
+
 
 }
+
+//Plot: Path through maturity and self understanding
+
+// Audio: Life begins with a stumble. 
+// Action: Ball starts to descend stairs located in the middle of the room
+
+// Audio: Just as this ball awkwardly descends the stairs, everyone begins their journey clumsily, relying on momentum and instinct over purposeful smooth decision making. 
+// Action: Ball reaches the water and starts to float without moving much
+
+// Audio: Everything is a first, but your destination is clear and although there may be bumps along your road, you know where you are going and there is a comfort in that.
+// Action: A waterfall located at the left of the player turns on and attracts the attention.
+
+// Audio: But once you’ve adjusted to the basics of life, adolescence begins and everything you thought you knew changes once again. 
+// Action: Due to the waterfall, ball starts to move in the opposite direction and head towards a tunnel
+
+// Audio: Your life can begin to drift in a direction you never expected and your destination can become more and more unclear. Who are you? Where are you going? The future is obscured and only by going forward can we find the answer to these questions
+// Action: Ball reaches the tunnel and “swirls” into the darkness. Only music and sound effects will occur during the following 10 seconds to generate suspense
+
+// Audio: Ambiguity cannot last forever. Eventually everyone comes to an understanding of who they are and what they want out of life. The darkness is gone and a clear road lies ahead. 
+// Action: Ball shows up at the other end of the tunnel and starts to move towards the player
+
+// Audio: There may be troubles ahead. Anything could happen. Will you be able to make your way through them? Once you know who you are, the tools to deal with whatever may come are right there for you. All you need … is to pick them up
+// Action: Ball reaches the player and they are able to pick it up. Either picking the ball up or 10 seconds pass after it reaches the player will end the experience
--- Assets/Scripts/Helper/BallAnimatorTool.cs
+++ Assets/Scripts/Helper/BallAnimatorTool.cs
@@ -8,9 +8,26 @@ public class BallAnimatorTool : MonoBehaviour
     [SerializeField] private Transform _ballTransform = null;
     [SerializeField] private List<Transform> _nodes = new List<Transform>();
 
+    [Header("Buoyancy")]
+    [SerializeField] private float _amplitude = 0.1f; // The maximum amount to move the object up and down
+    [SerializeField] private float _frequency = 3.5f; // The speed at which to move the object up and down
+    private float _startYPos; // The object's starting Y position
+
+    [Header("Rotaation")]
+    [SerializeField] private float _rotAmplitude = 0.1f; // The maximum amount to move the object up and down
+    [SerializeField] private float _rotFrequency = 3.5f; // The speed at which to move the object up and down
+    private float _startYRot; // The object's starting Y position
+
     private float _elapsedTime = 0.0f;
     private int _currentNodeIndex = 0;
 
+    private void Start()
+    {
+        _startYPos = _ballTransform.position.y; // Store the object's starting Y position
+        _startYRot = _ballTransform.localRotation.y; // Store the object's starting Y rotation
+    }
+
+    // Move to courotine
     void Update()
     {
         if (_canMove == false || _nodes.Count == 0)
@@ -35,8 +52,25 @@ public class BallAnimatorTool : MonoBehaviour
             _currentNodeIndex = (_currentNodeIndex + 1) % _nodes.Count;
         }
 
+        // Rotation
+        var ballRot = _ballTransform.localRotation;
+        ballRot.y = _startYRot + _rotAmplitude * Mathf.Sin(_rotFrequency * Time.time); // Calculate the new Y rotation using a sine wave to mimic x
+        _ballTransform.localRotation = ballRot;
+
+        // Position
         float t = Mathf.Clamp01(_elapsedTime / _totalTime);
-        _ballTransform.position = Vector3.Lerp(startPosition, endPosition, t) + transform.up * Mathf.Sin(t * Mathf.PI) * 0.1f;
+        Vector3 ballPos = Vector3.Lerp(startPosition, endPosition, t) + transform.up * Mathf.Sin(t * Mathf.PI) * 0.1f;
+        ballPos.y = _startYPos + _amplitude * Mathf.Sin(_frequency * Time.time); // Calculate the new Y position using a sine wave to mimic Buoyancy
+        _ballTransform.position = ballPos;
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector3 verticalOffset = Vector3.up * 1.0f;
+        for (int i = 0; i < _nodes.Count - 1; ++i)
+        {
+            Gizmos.DrawLine(_nodes[i].position + verticalOffset, _nodes[i + 1].position + verticalOffset);
+        }
+    }
 }
