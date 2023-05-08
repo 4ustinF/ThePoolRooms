edd08fb18ccee998847781c272fabb0d4ad81b8e
William Clark <Austin.farback9@gmail.com>
2023-05-08 14:58:26 -0700

    [Will Branch] - Finished Animator

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -31,7 +31,7 @@ public class GameManager : MonoBehaviour
 
 
     [Header("References")]
-    //[SerializeField] private WaterFallManager _waterFallManager = null;
+    [SerializeField] private WaterFallManager _waterFallManager = null;
     [SerializeField] private AudioSource _audioSource = null;
 
     //[Header("Ambience")]
@@ -109,19 +109,20 @@ public class GameManager : MonoBehaviour
     {
         switch (_currentEvent)
         {
-            case CurrentEvent.Event1:
+            case CurrentEvent.Event1: // Ball starts to descend stairs located in the middle of the room
                 break;
-            case CurrentEvent.Event2:
+            case CurrentEvent.Event2: // Ball reaches the water and starts to float without moving much
                 break;
-            case CurrentEvent.Event3:
+            case CurrentEvent.Event3: // A waterfall located at the left of the player turns on and attracts the attention.
+                _waterFallManager?.StartWaterFall();
                 break;
-            case CurrentEvent.Event4:
+            case CurrentEvent.Event4: // Due to the waterfall, ball starts to move in the opposite direction and head towards a tunnel
                 break;
-            case CurrentEvent.Event5:
+            case CurrentEvent.Event5: // Ball reaches the tunnel and “swirls” into the darkness. Only music and sound effects will occur during the following 10 seconds to generate suspense
                 break;
-            case CurrentEvent.Event6:
+            case CurrentEvent.Event6: // Ball shows up at the other end of the tunnel and starts to move towards the player
                 break;
-            case CurrentEvent.Event7:
+            case CurrentEvent.Event7: // Ball reaches the player and they are able to pick it up. Either picking the ball up or 10 seconds pass after it reaches the player will end the experience
                 break;
         }
 
@@ -134,23 +135,30 @@ public class GameManager : MonoBehaviour
 
 //Plot: Path through maturity and self understanding
 
+// Event1
 // Audio: Life begins with a stumble. 
 // Action: Ball starts to descend stairs located in the middle of the room
 
+// Event2
 // Audio: Just as this ball awkwardly descends the stairs, everyone begins their journey clumsily, relying on momentum and instinct over purposeful smooth decision making. 
 // Action: Ball reaches the water and starts to float without moving much
 
+// Event3
 // Audio: Everything is a first, but your destination is clear and although there may be bumps along your road, you know where you are going and there is a comfort in that.
 // Action: A waterfall located at the left of the player turns on and attracts the attention.
 
+// Event4
 // Audio: But once you’ve adjusted to the basics of life, adolescence begins and everything you thought you knew changes once again. 
 // Action: Due to the waterfall, ball starts to move in the opposite direction and head towards a tunnel
 
+// Event5
 // Audio: Your life can begin to drift in a direction you never expected and your destination can become more and more unclear. Who are you? Where are you going? The future is obscured and only by going forward can we find the answer to these questions
 // Action: Ball reaches the tunnel and “swirls” into the darkness. Only music and sound effects will occur during the following 10 seconds to generate suspense
 
+// Event6
 // Audio: Ambiguity cannot last forever. Eventually everyone comes to an understanding of who they are and what they want out of life. The darkness is gone and a clear road lies ahead. 
 // Action: Ball shows up at the other end of the tunnel and starts to move towards the player
 
+// Event7
 // Audio: There may be troubles ahead. Anything could happen. Will you be able to make your way through them? Once you know who you are, the tools to deal with whatever may come are right there for you. All you need … is to pick them up
 // Action: Ball reaches the player and they are able to pick it up. Either picking the ball up or 10 seconds pass after it reaches the player will end the experience
--- Assets/Scripts/Helper/BallAnimatorTool.cs
+++ Assets/Scripts/Helper/BallAnimatorTool.cs
@@ -1,67 +1,76 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BallAnimatorTool : MonoBehaviour
 {
-    [SerializeField] private bool _canMove = false;
     [SerializeField] private float _totalTime = 5.0f;
     [SerializeField] private Transform _ballTransform = null;
     [SerializeField] private List<Transform> _nodes = new List<Transform>();
+
+    [Header("Rotation")]
+    [SerializeField] private float _rotAmplitude = 0.2f; // The maximum amount to move the object up and down
+    [SerializeField] private float _rotFrequency = 0.5f; // The speed at which to move the object up and down
+    private float _startYRot; // The object's starting Y position
 
     [Header("Buoyancy")]
     [SerializeField] private float _amplitude = 0.1f; // The maximum amount to move the object up and down
     [SerializeField] private float _frequency = 3.5f; // The speed at which to move the object up and down
     private float _startYPos; // The object's starting Y position
 
-    [Header("Rotaation")]
-    [SerializeField] private float _rotAmplitude = 0.1f; // The maximum amount to move the object up and down
-    [SerializeField] private float _rotFrequency = 3.5f; // The speed at which to move the object up and down
-    private float _startYRot; // The object's starting Y position
-
-    private float _elapsedTime = 0.0f;
-    private int _currentNodeIndex = 0;
-
     private void Start()
     {
-        _startYPos = _ballTransform.position.y; // Store the object's starting Y position
         _startYRot = _ballTransform.localRotation.y; // Store the object's starting Y rotation
+        _startYPos = _ballTransform.position.y; // Store the object's starting Y position
     }
 
-    // Move to courotine
-    void Update()
+    public void StartAnim()
     {
-        if (_canMove == false || _nodes.Count == 0)
+        StartCoroutine(AnimRoutine(0, 1));
+    }
+
+    private IEnumerator AnimRoutine(int startIndex, int endIndex)
+    {
+        float elapsedTime = 0.0f;
+        Vector3 startPosition = _nodes[startIndex].position;
+        Vector3 endPosition = _nodes[endIndex].position;
+
+        while (true)
         {
-            return;
+            elapsedTime += Time.deltaTime;
+
+            // Rotation
+            var ballRot = _ballTransform.localRotation;
+            ballRot.y = _startYRot + _rotAmplitude * Mathf.Sin(_rotFrequency * Time.time); // Calculate the new Y rotation using a sine wave to mimic x
+            _ballTransform.localRotation = ballRot;
+
+            // Position
+            float t = Mathf.Clamp01(elapsedTime / _totalTime);
+            Vector3 ballPos = Vector3.Lerp(startPosition, endPosition, t) + transform.up * Mathf.Sin(t * Mathf.PI) * 0.1f;
+            ballPos.y = _startYPos + _amplitude * Mathf.Sin(_frequency * Time.time); // Calculate the new Y position using a sine wave to mimic Buoyancy
+            _ballTransform.position = ballPos;
+
+            if (elapsedTime >= _totalTime)
+            {
+                break;
+            }
+
+            yield return null;
         }
 
-        Transform currentNode = _nodes[_currentNodeIndex];
-        Transform nextNode = _nodes[(_currentNodeIndex + 1) % _nodes.Count];
-
-        Vector3 startPosition = currentNode.position;
-        Vector3 endPosition = nextNode.position;
-
-        float distance = Vector3.Distance(startPosition, endPosition);
-        float speed = distance / _totalTime;
-
-        _elapsedTime += Time.deltaTime;
-
-        if (_elapsedTime >= _totalTime)
+        if (endIndex < _nodes.Count - 1)
         {
-            _elapsedTime = 0.0f;
-            _currentNodeIndex = (_currentNodeIndex + 1) % _nodes.Count;
+            StartCoroutine(AnimRoutine(startIndex + 1, endIndex + 1));
         }
+    }
 
-        // Rotation
-        var ballRot = _ballTransform.localRotation;
-        ballRot.y = _startYRot + _rotAmplitude * Mathf.Sin(_rotFrequency * Time.time); // Calculate the new Y rotation using a sine wave to mimic x
-        _ballTransform.localRotation = ballRot;
-
-        // Position
-        float t = Mathf.Clamp01(_elapsedTime / _totalTime);
-        Vector3 ballPos = Vector3.Lerp(startPosition, endPosition, t) + transform.up * Mathf.Sin(t * Mathf.PI) * 0.1f;
-        ballPos.y = _startYPos + _amplitude * Mathf.Sin(_frequency * Time.time); // Calculate the new Y position using a sine wave to mimic Buoyancy
-        _ballTransform.position = ballPos;
+    public void ObtainNodes()
+    {
+        _nodes.Clear();
+        foreach (Transform child in transform)
+        {
+            _nodes.Add(child);
+        }
     }
 
     private void OnDrawGizmos()
