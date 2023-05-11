fe3b361cb7010a3cbe20510b00a8b27c3c2b5ee9
TheSolomonster <Aidan@Solomonster.org>
2023-05-10 19:27:08 -0700

    [Aidan Branch] Subtitles p2

--- Assets/Scripts/Editor/SubtitleEditor.cs
+++ Assets/Scripts/Editor/SubtitleEditor.cs
@@ -13,5 +13,33 @@ public class SubtitleEditor : Editor
         {
             script.ReadNextLine();
         }
+        if (GUILayout.Button("Read Line 1", GUILayout.Height(30)))
+        {
+            script.ReadSpecificLine(1);
+        }
+        if (GUILayout.Button("Read Line 2", GUILayout.Height(30)))
+        {
+            script.ReadSpecificLine(2);
+        }
+        if (GUILayout.Button("Read Line 3", GUILayout.Height(30)))
+        {
+            script.ReadSpecificLine(3);
+        }
+        if (GUILayout.Button("Read Line 4", GUILayout.Height(30)))
+        {
+            script.ReadSpecificLine(4);
+        }
+        if (GUILayout.Button("Read Line 5", GUILayout.Height(30)))
+        {
+            script.ReadSpecificLine(5);
+        }
+        if (GUILayout.Button("Read Line 6", GUILayout.Height(30)))
+        {
+            script.ReadSpecificLine(6);
+        }
+        if (GUILayout.Button("Read Line 7", GUILayout.Height(30)))
+        {
+            script.ReadSpecificLine(7);
+        }
     }
 }
--- Assets/Scripts/Systems/SubtitleManager.cs
+++ Assets/Scripts/Systems/SubtitleManager.cs
@@ -3,11 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+
+struct Subtitle
+{
+    public string text;
+    public float seconds;
+    public Subtitle(string Text, float Seconds)
+    {
+        text = Text;
+        seconds = Seconds;
+    }
+}
+
 public class SubtitleManager : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _tutorialText = null;
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI _subtitleText = null;
+
+    [Header("Variables")]
+    [SerializeField] private float _textSpeed = 1.5f;
+    [SerializeField] private float _dialouge1Length = 4f;
+    [SerializeField] private float _dialouge2aLength = 8f;
+    [SerializeField] private float _dialouge2bLength = 8f;
+    [SerializeField] private float _dialouge3aLength = 10f;
+    [SerializeField] private float _dialouge3bLength = 6f;
+    [SerializeField] private float _dialouge4Length = 12f;
+    [SerializeField] private float _dialouge5aLength = 10f;
+    [SerializeField] private float _dialouge5bLength = 4f;
+    [SerializeField] private float _dialouge5cLength = 10f;
+    [SerializeField] private float _dialouge6aLength = 12f;
+    [SerializeField] private float _dialouge6bLength = 5f;
+    [SerializeField] private float _dialouge7aLength = 4f;
+    [SerializeField] private float _dialouge7bLength = 8f;
+    [SerializeField] private float _dialouge7cLength = 6f;
+
     private Subtitle _currentSubtitle;
-    private List<List<Subtitle>> _tutorials;
+    private List<List<Subtitle>> _subtitles;
     private int _currentLine = 0;
     private int _currentLinePart = 0;
     private int _characterProgress = 1;
@@ -16,89 +47,139 @@ public class SubtitleManager : MonoBehaviour
     private float _timePerLine = 0f;
     private float _timeSinceLastLine = 0f;
     private float _timeLineHasPlayed = 0f;
-    private 
 
-    struct Subtitle
-    {
-        public string text;
-        public float seconds;
-        public Subtitle(string Text, int Seconds)
-        {
-            text = Text;
-            seconds = Seconds;
-        }
-    }
-
+    private float _frame = 1 / 60f;
 
     public void Start()
     {
-        _tutorials = new List<List<Subtitle>>();
+        _subtitles = new List<List<Subtitle>>();
         for (int i = 0; i < 7; i++)
         {
-            _tutorials.Add(new List<Subtitle>());
+            _subtitles.Add(new List<Subtitle>());
         }
-        _tutorials[0].Add(new Subtitle("Life begins with a stumble", 4));
-        _tutorials[1].Add(new Subtitle("Just as this ball awkwardly descends the stairs, everyone begins their journey clumsily,", 8));
-        _tutorials[1].Add(new Subtitle("relying on momentum and instinct over purposeful smooth decision making.", 8));
-        _tutorials[2].Add(new Subtitle("Everything is a first, but your destination is clear and although there may be bumps along your road,", 10));
-        _tutorials[2].Add(new Subtitle("along your road, you know where you are going and there is a comfort in that.", 6));
-        _tutorials[3].Add(new Subtitle("But once you’ve adjusted to the basics of life, adolescence begins and everything you thought you knew changes once again.", 12));
-        _tutorials[4].Add(new Subtitle("Your life can begin to drift in a direction you never expected and your destination can become more and more unclear.", 10));
-        _tutorials[4].Add(new Subtitle("Who are you? Where are you going?", 4));
-        _tutorials[4].Add(new Subtitle("The future is obscured and only by going forward can we find the answer to these questions.", 10));
-        _tutorials[5].Add(new Subtitle("But ambiguity cannot last forever. Eventually everyone comes to an understanding of who they are and what they want out of life.", 12));
-        _tutorials[5].Add(new Subtitle("The darkness is gone and a clear road lies ahead.", 5));
-        _tutorials[6].Add(new Subtitle("Will you be able to make your way through them?", 4));
-        _tutorials[6].Add(new Subtitle("Once you know who you are, the tools to deal with whatever may come are right there for you.", 8));
-        _tutorials[6].Add(new Subtitle("All you need … is to pick them up", 6));
-        _tutorialText.text = "";
+        _subtitles[0].Add(new Subtitle("Life begins with a stumble", _dialouge1Length));
+        _subtitles[1].Add(new Subtitle("Just as this ball awkwardly descends the stairs, everyone begins their journey clumsily,", _dialouge2aLength));
+        _subtitles[1].Add(new Subtitle("Relying on momentum and instinct over purposeful smooth decision making.", _dialouge2bLength));
+        _subtitles[2].Add(new Subtitle("Everything is a first, but your destination is clear and although there may be bumps along your road,", _dialouge3aLength));
+        _subtitles[2].Add(new Subtitle("You know where you are going and there is a comfort in that.", _dialouge3bLength));
+        _subtitles[3].Add(new Subtitle("But once you’ve adjusted to the basics of life, adolescence begins and everything you thought you knew changes once again.", _dialouge4Length));
+        _subtitles[4].Add(new Subtitle("Your life can begin to drift in a direction you never expected and your destination can become more and more unclear.", _dialouge5aLength));
+        _subtitles[4].Add(new Subtitle("Who are you? Where are you going?", _dialouge5bLength));
+        _subtitles[4].Add(new Subtitle("The future is obscured and only by going forward can we find the answer to these questions.", _dialouge5cLength));
+        _subtitles[5].Add(new Subtitle("But ambiguity cannot last forever. Eventually everyone comes to an understanding of who they are and what they want out of life.", _dialouge6aLength));
+        _subtitles[5].Add(new Subtitle("The darkness is gone and a clear road lies ahead.", _dialouge6bLength));
+        _subtitles[6].Add(new Subtitle("Will you be able to make your way through them?", _dialouge7aLength));
+        _subtitles[6].Add(new Subtitle("Once you know who you are, the tools to deal with whatever may come are right there for you.", _dialouge7bLength));
+        _subtitles[6].Add(new Subtitle("All you need … is to pick them up", _dialouge7cLength));
+        _subtitleText.text = "";
     }
 
     public void ReadNextLine()
     {
-        _readLine = true;
+        if (_readLine == true)
+        {
+            if (_subtitles[_currentLine].Count - 1 == _currentLinePart)
+            {
+                _currentLine++;
+                _currentLinePart = 0;
+            }
+            else
+            {
+                _currentLinePart++;
+            }
+        }
+        else
+        {
+            _readLine = true;
+        }
+        _startedReadingLine = false;
     }
 
-    void Update()
+    public void ReadSpecificLine(int lineNum)
+    {
+        if (lineNum - 1 > 6)
+        {
+            return;
+        }
+
+        if (_readLine == true)
+        {
+            if (_subtitles[_currentLine].Count - 1 == _currentLinePart)
+            {
+                _currentLine++;
+                _currentLinePart = 0;
+            }
+            else
+            {
+                _currentLinePart++;
+            }
+        }
+        else
+        {
+            _readLine = true;
+        }
+        _startedReadingLine = false;
+        _currentLine = lineNum-1;
+        _currentLinePart = 0;
+    }
+
+    void FixedUpdate()
     {
         if (_readLine == false)
         {
             return;
         }
 
+        //early return at max line count
+        if (_currentLine > 6)
+        {
+            _subtitleText.text = "";
+            _readLine = false;
+            return;
+        }
+
         if (_startedReadingLine == false)
         {
-            _currentSubtitle = _tutorials[_currentLine][_currentLinePart];
+            _currentSubtitle = _subtitles[_currentLine][_currentLinePart];
             _startedReadingLine = true;
             _characterProgress = 1;
             _timeSinceLastLine = 0f;
             _timeLineHasPlayed = 0f;
-            _tutorialText.color = new Color(_tutorialText.color.r, _tutorialText.color.g, _tutorialText.color.b, 1);
-            _timePerLine = (_currentSubtitle.seconds - 2) / _currentSubtitle.text.Length;
+            _subtitleText.color = new Color(_subtitleText.color.r, _subtitleText.color.g, _subtitleText.color.b, 1);
+            if(_currentSubtitle.seconds - _textSpeed <= 0)
+            {
+                _timePerLine = .1f;
+            }
+            else
+            {
+                _timePerLine = (_currentSubtitle.seconds - _textSpeed) / _currentSubtitle.text.Length;
+            }
             ShowText();
         }
         else
         {
-            _timeSinceLastLine += Time.deltaTime;
-            _timeLineHasPlayed += Time.deltaTime;
+            _timeSinceLastLine += _frame;
+            _timeLineHasPlayed += _frame;
             if (_timeSinceLastLine > _timePerLine)
             {
                 _timeSinceLastLine = 0f;
                 ShowText();
             }
+
             if (_timeLineHasPlayed > _currentSubtitle.seconds)
             {
                 _readLine = false;
                 _startedReadingLine = false;
-                StartCoroutine(FadeTextToZeroAlpha(2f));
-                if (_tutorials[_currentLine].Count - 1 == _currentLinePart)
+                if (_subtitles[_currentLine].Count - 1 == _currentLinePart)
                 {
                     _currentLine++;
                     _currentLinePart = 0;
+                    StartCoroutine(FadeTextToZeroAlpha(2f));
                 }
                 else
                 {
                     _currentLinePart++;
+                    StartCoroutine(FadeTextToZeroAlpha(1f));
                 }
             }
         }
@@ -106,15 +187,21 @@ public class SubtitleManager : MonoBehaviour
 
     public IEnumerator FadeTextToZeroAlpha(float t)
     {
-        _tutorialText.color = new Color(_tutorialText.color.r, _tutorialText.color.g, _tutorialText.color.b, 1);
-        while (_tutorialText.color.a > 0.0f)
+        _subtitleText.color = new Color(_subtitleText.color.r, _subtitleText.color.g, _subtitleText.color.b, 1f);
+        while (_subtitleText.color.a > 0.0f)
         {
             if(_readLine == true)
             {
                 yield break;
             }
-            _tutorialText.color = new Color(_tutorialText.color.r, _tutorialText.color.g, _tutorialText.color.b, _tutorialText.color.a - (Time.deltaTime / t));
+
+            _subtitleText.color = new Color(_subtitleText.color.r, _subtitleText.color.g, _subtitleText.color.b, _subtitleText.color.a - (Time.deltaTime / t));
             yield return null;
+        }
+
+        if(_currentLinePart > 0)
+        {
+            _readLine = true;
         }
     }
 
@@ -122,12 +209,13 @@ public class SubtitleManager : MonoBehaviour
     {
         if (_characterProgress >= _currentSubtitle.text.Length)
         {
-            _tutorialText.text = _currentSubtitle.text;
+            _subtitleText.text = _currentSubtitle.text;
             return;
         }
+
         string visible = _currentSubtitle.text.Substring(0, _characterProgress);
         string notVisible = _currentSubtitle.text.Substring(_characterProgress, Mathf.Max(_currentSubtitle.text.Length - (_characterProgress), 0));
-        _tutorialText.text = $"{visible}<color=#00000000>{notVisible}</color>";
+        _subtitleText.text = $"{visible}<color=#00000000>{notVisible}</color>";
         _characterProgress++;
     }
 }
