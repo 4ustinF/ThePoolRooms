90357a5f50bc8fdfa8998f9fa31babc73a5967c4
William Clark <Austin.farback9@gmail.com>
2023-05-28 17:22:54 -0700

    [Will Branch] - Adjusted Main Scene
    - Added bounce sfx
    - Added water drip ambience
    - Added drip prefab to main scene
    - Adjusted ivy prefab

--- Assets/Scripts/GamePlay/BallManager.cs
+++ Assets/Scripts/GamePlay/BallManager.cs
@@ -4,7 +4,9 @@ public class BallManager : MonoBehaviour
 {
     [SerializeField] private GameObject _ripples = null;
     [SerializeField] private ParticleSystem _waterSplashPS = null;
-    [SerializeField] private AudioSource _splashAudioSource = null;
+    [SerializeField] private AudioSource _audioSource = null;
+    [SerializeField] private AudioClip _splashAudioClip = null;
+    [SerializeField] private AudioClip _ballBounceAudioClip = null;
 
     private void PlayWaterSplash()
     {
@@ -12,8 +14,10 @@ public class BallManager : MonoBehaviour
         _waterSplashPS.transform.rotation = Quaternion.FromToRotation(_waterSplashPS.transform.eulerAngles, Vector3.up);
         _waterSplashPS.gameObject.SetActive(true);
 
-        _splashAudioSource.pitch = Random.Range(0.85f, 1.15f);
-        _splashAudioSource.Play();
+        _audioSource.pitch = Random.Range(0.85f, 1.15f);
+        _audioSource.volume = 0.15f;
+        _audioSource.clip = _splashAudioClip;
+        _audioSource.Play();
 
         _waterSplashPS.Play();
     }
@@ -21,6 +25,14 @@ public class BallManager : MonoBehaviour
     private void TurnOnRippleParticles()
     {
         _ripples.SetActive(true);
+    }
+
+    private void PlayBounce()
+    {
+        _audioSource.pitch = Random.Range(0.85f, 1.15f);
+        _audioSource.volume = 0.07f;
+        _audioSource.clip = _ballBounceAudioClip;
+        _audioSource.Play();
     }
 
 
