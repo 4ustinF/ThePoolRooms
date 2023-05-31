34689ba6b775ae6021b66b0379fde535e12f20af
William Clark <Austin.farback9@gmail.com>
2023-05-30 22:58:11 -0700

    [Will Branch] - Continued logic on water interaction

--- Assets/Scripts/GamePlay/WaterHandInteraction.cs
+++ Assets/Scripts/GamePlay/WaterHandInteraction.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class WaterHandInteraction : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _waterSplashes = new List<GameObject>();
+    [SerializeField] private List<GameObject> _waterRipples = new List<GameObject>();
     [SerializeField] private AudioSource _audioSource = null;
 
     private void OnTriggerEnter(Collider other)
@@ -13,15 +14,25 @@ public class WaterHandInteraction : MonoBehaviour
         if (string.Compare(other.tag, "Player", System.StringComparison.OrdinalIgnoreCase) == 0)
         {
             _audioSource.Play();
+            OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
+            OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.LTouch);
+
             foreach (GameObject p in _waterSplashes)
             {
                 if (p.activeInHierarchy == false)
                 {
-                    OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
-                    OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.LTouch);
-
                     p.transform.position = new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z);
                     p.SetActive(true);
+                    break;
+                }
+            }
+
+            foreach (GameObject ripple in _waterRipples)
+            {
+                if (ripple.activeInHierarchy == false)
+                {
+                    ripple.transform.position = new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z);
+                    ripple.SetActive(true);
                     break;
                 }
             }
