8dafe222f5b844f426891016bc068c53de84d82d
TheSolomonster <Aidan@Solomonster.org>
2023-06-06 12:08:00 -0700

    [Aidan Branch] Bumping up the time

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -109,7 +109,7 @@ public class GameManager : MonoBehaviour
         Debug.Log("PlayDialog1");
         _audioSource.PlayOneShot(_dialougeClip1);
 
-        float waitTime = _dialougeClip1.length + 1f;
+        float waitTime = _dialougeClip1.length + 2f;
         StartCoroutine(PlaySubtitle(1, 1, 2.0f, 0.5f));
 
         StartCoroutine(WaitAndInvokeFunc(waitTime, PlayBallFallingAnim));
@@ -120,12 +120,12 @@ public class GameManager : MonoBehaviour
     {
         _audioSource.PlayOneShot(_dialougeClip2);
 
-        float time1 = 5.25f;
-        float time2 = time1 + 0.75f;
+        float time1 = 6.25f;
+        float time2 = time1 + 1f;
         StartCoroutine(PlaySubtitle(2, 1, time1, 0.5f));
         StartCoroutine(PlaySubtitle(2, 2, 5.0f, time2));
 
-        StartCoroutine(WaitAndInvokeFunc(_dialougeClip2.length + 2.0f, PlayDialog3));
+        StartCoroutine(WaitAndInvokeFunc(_dialougeClip2.length + 3.0f, PlayDialog3));
     }
 
     private void PlayDialog3()
@@ -134,9 +134,9 @@ public class GameManager : MonoBehaviour
         StartCoroutine(WaitAndInvokeAudio(_dialougeClip3.length + 0.25f, _dialougeClip4));
 
         StartCoroutine(PlaySubtitle(3, 1, _dialougeClip3.length - 2.0f, 0.5f));
-        StartCoroutine(PlaySubtitle(3, 2, _dialougeClip4.length - 2.0f, _dialougeClip3.length + 0.75f));
+        StartCoroutine(PlaySubtitle(3, 2, _dialougeClip4.length - 2.0f, _dialougeClip3.length + 1.75f));
 
-        float waitTime = _dialougeClip3.length + _dialougeClip4.length + 5.0f;
+        float waitTime = _dialougeClip3.length + _dialougeClip4.length + 7.0f;
         StartCoroutine(WaitAndInvokeFunc(waitTime + 0.25f, TurnOnWaterFall));
         StartCoroutine(WaitAndInvokeFunc(waitTime + 0.25f, PlayDialog4));
     }
@@ -147,7 +147,7 @@ public class GameManager : MonoBehaviour
 
         _subtitleManager?.ReadSpecificLine(4, 1, _dialougeClip5.length - 2.0f);
 
-        StartCoroutine(WaitAndInvokeFunc(_dialougeClip4.length + 5.25f, PlayDialog5));
+        StartCoroutine(WaitAndInvokeFunc(_dialougeClip4.length + 6f, PlayDialog5));
     }
 
     private void PlayDialog5()
@@ -159,11 +159,11 @@ public class GameManager : MonoBehaviour
         _subtitleManager?.ReadSpecificLine(5, 1, _dialougeClip6.length - 2f);
         StartCoroutine(PlaySubtitle(5, 2, _dialougeClip7.length - 2f, _dialougeClip6.length + 0.25f));
         StartCoroutine(PlaySubtitle(5, 3, 6.0f, _dialougeClip7.length + _dialougeClip6.length + 0.5f));
-        StartCoroutine(PlaySubtitle(5, 4, 7.0f, _dialougeClip7.length + _dialougeClip6.length + 7.5f));
+        StartCoroutine(PlaySubtitle(5, 4, 7.0f, _dialougeClip7.length + _dialougeClip6.length + 8.5f));
 
-        StartCoroutine(WaitAndInvokeFunc(_dialougeClip7.length + _dialougeClip6.length + 7.5f, _fishManager.StartFishParticles));
+        StartCoroutine(WaitAndInvokeFunc(_dialougeClip7.length + _dialougeClip6.length + 8.5f, _fishManager.StartFishParticles));
 
-        float waitTime = _dialougeClip6.length + _dialougeClip7.length + _dialougeClip8.length + 6.0f + 10.0f;
+        float waitTime = _dialougeClip6.length + _dialougeClip7.length + _dialougeClip8.length + 17.0f;
         StartCoroutine(WaitAndInvokeFunc(waitTime, _waterFallManager.StopWaterFall));
         StartCoroutine(WaitAndInvokeFunc(waitTime, _fishManager.StopFishParticles));
         StartCoroutine(WaitAndInvokeFunc(waitTime + 0.5f, PlayDialog6));
@@ -175,9 +175,9 @@ public class GameManager : MonoBehaviour
         StartCoroutine(WaitAndInvokeAudio(_dialougeClip9.length + 0.25f, _dialougeClip10));
 
         StartCoroutine(PlaySubtitle(6, 1, _dialougeClip9.length - 2.0f, 0.5f));
-        StartCoroutine(PlaySubtitle(6, 2, _dialougeClip10.length - 2.0f, _dialougeClip9.length + 0.25f));
+        StartCoroutine(PlaySubtitle(6, 2, _dialougeClip10.length - 2.0f, _dialougeClip9.length + 1.25f));
 
-        float waitTime = _dialougeClip9.length + _dialougeClip10.length + 5.25f;
+        float waitTime = _dialougeClip9.length + _dialougeClip10.length + 6.25f;
         StartCoroutine(WaitAndInvokeFunc(waitTime + 0.25f, PlayBallExitTunnel));
         StartCoroutine(WaitAndInvokeFunc(waitTime + 0.25f, PlayDialog7));
     }
@@ -191,7 +191,7 @@ public class GameManager : MonoBehaviour
         StartCoroutine(PlaySubtitle(7, 1, _dialougeClip11.length - 2f, 0.5f));
         StartCoroutine(PlaySubtitle(7, 2, _dialougeClip12.length - 2f, _dialougeClip11.length + 0.25f));
         StartCoroutine(PlaySubtitle(7, 3, 6.0f, _dialougeClip11.length + _dialougeClip12.length + 0.5f));
-        StartCoroutine(PlaySubtitle(7, 4, 4.0f, _dialougeClip11.length + _dialougeClip12.length + 8.5f));
+        StartCoroutine(PlaySubtitle(7, 4, 4.0f, _dialougeClip11.length + _dialougeClip12.length + 9.5f));
 
         //StartCoroutine(WaitAndInvokeFunc(_dialougeClip7.length, _waterFallManager.StopWaterFall));
     }
