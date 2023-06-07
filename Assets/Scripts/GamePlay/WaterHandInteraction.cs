35130d2fa559d6350fcdc72462128877b246bc17
William Clark <Austin.farback9@gmail.com>
2023-06-07 00:56:45 -0700

    Update WaterHandInteraction.cs

--- Assets/Scripts/GamePlay/WaterHandInteraction.cs
+++ Assets/Scripts/GamePlay/WaterHandInteraction.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Liminal.SDK.Core;
+using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -8,9 +11,22 @@ public class WaterHandInteraction : MonoBehaviour, IPointerClickHandler
     [SerializeField] private List<GameObject> _waterRipples = new List<GameObject>();
     [SerializeField] private AudioSource _audioSource = null;
 
+    private float _frequency = 0.5f;
+    private float _amplitude = 0.5f;
+
     public void OnPointerClick(PointerEventData eventData)
     {
         SplashWater(eventData.pointerCurrentRaycast.worldPosition);
+
+        //Debug.Log(eventData.pointerId);
+        //if(eventData.pointerId == 0)
+        //{
+        //    OVRInput.SetControllerVibration(_frequency, _amplitude, OVRInput.Controller.LTouch);
+        //}
+        //else if(eventData.pointerId == 1) 
+        //{
+        //    OVRInput.SetControllerVibration(_frequency, _amplitude, OVRInput.Controller.RTouch);
+        //}
     }
 
     private void SplashWater(Vector3 pos)
@@ -39,5 +55,5 @@ public class WaterHandInteraction : MonoBehaviour, IPointerClickHandler
     }
 }
 
-// OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
-// OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.LTouch);
+// OVRInput.SetControllerVibration(_frequency, _amplitude, OVRInput.Controller.RTouch);
+// OVRInput.SetControllerVibration(_frequency, _amplitude, OVRInput.Controller.LTouch);
