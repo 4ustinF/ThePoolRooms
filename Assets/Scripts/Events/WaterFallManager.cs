7a3e6c4ad5b88a8a82a0cfe739a61679e3b13a76
William Clark <Austin.farback9@gmail.com>
2023-06-13 17:56:30 -0700

    [Will Branch] - Made waterfall less extreme

--- Assets/Scripts/Events/WaterFallManager.cs
+++ Assets/Scripts/Events/WaterFallManager.cs
@@ -5,14 +5,13 @@ public class WaterFallManager : MonoBehaviour, IGameEvent
 {
     [SerializeField] private AudioSource _audioSource = null;
     [SerializeField] private WaterfallParticle _waterFallPS = null;
-    [SerializeField] private WaterfallParticle _dustFallPS = null;
+    //[SerializeField] private WaterfallParticle _dustFallPS = null;
     [SerializeField] private WaterfallParticle _sprinklesPS = null;
-    [SerializeField] private WaterfallParticle _surfaceDust1PS = null;
+    //[SerializeField] private WaterfallParticle _surfaceDust1PS = null;
     [SerializeField] private WaterfallParticle _surfaceDust2PS = null;
-    [SerializeField] private float _maxVolume = 0.25f;
-    [SerializeField] private float _fadeInMultiplier = 0.75f;
-    [SerializeField] private float _fadeOutMultiplier = 0.5f;
-    [SerializeField] private float _waitTime = 1.5f;
+    [SerializeField] private float _maxVolume = 0.15f;
+    [SerializeField] private float _fadeInTime = 3.0f;
+    [SerializeField] private float _fadeOutTime = 1.4f;
 
     public void StartGameEvent()
     {
@@ -35,13 +34,13 @@ public class WaterFallManager : MonoBehaviour, IGameEvent
     private IEnumerator FadeInWaterFalling()
     {
         var _waterFallEmission = _waterFallPS.GetAndSetEmission(0.0f);
-        var _dustFallEmission = _dustFallPS.GetAndSetEmission(0.0f);
+        //var _dustFallEmission = _dustFallPS.GetAndSetEmission(0.0f);
 
         _waterFallPS.particleSystem.Play();
-        _dustFallPS.particleSystem.Play();
+        //_dustFallPS.particleSystem.Play();
 
         float timer = 0.0f;
-        float maxTime = 2.0f;
+        float maxTime = 4.0f;
 
         while (timer < maxTime)
         {
@@ -49,25 +48,25 @@ public class WaterFallManager : MonoBehaviour, IGameEvent
             float percent = timer / maxTime;
 
             _waterFallEmission.rateOverTime = Mathf.Lerp(0.0f, _waterFallPS.maxEmissionRate, percent);
-            _dustFallEmission.rateOverTime = Mathf.Lerp(0.0f, _dustFallPS.maxEmissionRate, percent);
+            //_dustFallEmission.rateOverTime = Mathf.Lerp(0.0f, _dustFallPS.maxEmissionRate, percent);
 
             yield return null;
         }
 
         _waterFallEmission.rateOverTime = _waterFallPS.maxEmissionRate;
-        _dustFallEmission.rateOverTime = _dustFallPS.maxEmissionRate;
+        //_dustFallEmission.rateOverTime = _dustFallPS.maxEmissionRate;
     }
 
     private IEnumerator FadeOutWaterFalling()
     {
         var _waterFallEmission = _waterFallPS.GetAndSetEmission(_waterFallPS.maxEmissionRate);
-        var _dustFallEmission = _dustFallPS.GetAndSetEmission(_dustFallPS.maxEmissionRate);
+        //var _dustFallEmission = _dustFallPS.GetAndSetEmission(_dustFallPS.maxEmissionRate);
         var _sprinklesEmission = _sprinklesPS.GetAndSetEmission(_sprinklesPS.maxEmissionRate);
-        var _surfaceDustEmission = _surfaceDust1PS.GetAndSetEmission(_surfaceDust1PS.maxEmissionRate);
+        //var _surfaceDustEmission = _surfaceDust1PS.GetAndSetEmission(_surfaceDust1PS.maxEmissionRate);
         var _surfaceDus2Emission = _surfaceDust2PS.GetAndSetEmission(_surfaceDust2PS.maxEmissionRate);
 
         float timer = 0.0f;
-        float maxTime = 1.0f;
+        float maxTime = 1.5f;
 
         while (timer < maxTime)
         {
@@ -75,24 +74,24 @@ public class WaterFallManager : MonoBehaviour, IGameEvent
             float percent = timer / maxTime;
 
             _waterFallEmission.rateOverTime = Mathf.Lerp(_waterFallPS.maxEmissionRate, 0.0f, percent);
-            _dustFallEmission.rateOverTime = Mathf.Lerp(_dustFallPS.maxEmissionRate, 0.0f, percent);
+            //_dustFallEmission.rateOverTime = Mathf.Lerp(_dustFallPS.maxEmissionRate, 0.0f, percent);
             _sprinklesEmission.rateOverTime = Mathf.Lerp(_sprinklesPS.maxEmissionRate, 0.0f, percent);
-            _surfaceDustEmission.rateOverTime = Mathf.Lerp(_surfaceDust1PS.maxEmissionRate, 0.0f, percent);
+            //_surfaceDustEmission.rateOverTime = Mathf.Lerp(_surfaceDust1PS.maxEmissionRate, 0.0f, percent);
             _surfaceDus2Emission.rateOverTime = Mathf.Lerp(_surfaceDust2PS.maxEmissionRate, 0.0f, percent);
 
             yield return null;
         }
 
         _waterFallEmission.rateOverTime = 0.0f;
-        _dustFallEmission.rateOverTime = 0.0f;
+        //_dustFallEmission.rateOverTime = 0.0f;
         _sprinklesEmission.rateOverTime = 0.0f;
-        _surfaceDustEmission.rateOverTime = 0.0f;
+        //_surfaceDustEmission.rateOverTime = 0.0f;
         _surfaceDus2Emission.rateOverTime = 0.0f;
 
         _waterFallPS.particleSystem.Stop();
-        _dustFallPS.particleSystem.Stop();
+        //_dustFallPS.particleSystem.Stop();
         _sprinklesPS.particleSystem.Stop();
-        _surfaceDust1PS.particleSystem.Stop();
+        //_surfaceDust1PS.particleSystem.Stop();
         _surfaceDust2PS.particleSystem.Stop();
     }
 
@@ -101,15 +100,15 @@ public class WaterFallManager : MonoBehaviour, IGameEvent
         yield return new WaitForSeconds(0.5f);
 
         var _sprinklesEmission = _sprinklesPS.GetAndSetEmission(0.0f);
-        var _surfaceDustEmission = _surfaceDust1PS.GetAndSetEmission(0.0f);
+        //var _surfaceDustEmission = _surfaceDust1PS.GetAndSetEmission(0.0f);
         var _surfaceDus2Emission = _surfaceDust2PS.GetAndSetEmission(0.0f);
 
         _sprinklesPS.particleSystem.Play();
-        _surfaceDust1PS.particleSystem.Play();
+        //_surfaceDust1PS.particleSystem.Play();
         _surfaceDust2PS.particleSystem.Play();
 
         float timer = 0.0f;
-        float maxTime = 2.0f;
+        float maxTime = 3.0f;
 
         while (timer < maxTime)
         {
@@ -117,14 +116,14 @@ public class WaterFallManager : MonoBehaviour, IGameEvent
             float percent = timer / maxTime;
 
             _sprinklesEmission.rateOverTime = Mathf.Lerp(0.0f, _sprinklesPS.maxEmissionRate, percent);
-            _surfaceDustEmission.rateOverTime = Mathf.Lerp(0.0f, _surfaceDust1PS.maxEmissionRate, percent);
+            //_surfaceDustEmission.rateOverTime = Mathf.Lerp(0.0f, _surfaceDust1PS.maxEmissionRate, percent);
             _surfaceDus2Emission.rateOverTime = Mathf.Lerp(0.0f, _surfaceDust2PS.maxEmissionRate, percent);
 
             yield return null;
         }
 
         _sprinklesEmission.rateOverTime = _sprinklesPS.maxEmissionRate;
-        _surfaceDustEmission.rateOverTime = _surfaceDust1PS.maxEmissionRate;
+        //_surfaceDustEmission.rateOverTime = _surfaceDust1PS.maxEmissionRate;
         _surfaceDus2Emission.rateOverTime = _surfaceDust2PS.maxEmissionRate;
     }
 
@@ -135,7 +134,7 @@ public class WaterFallManager : MonoBehaviour, IGameEvent
 
         while (_audioSource.volume < _maxVolume)
         {
-            _audioSource.volume += _fadeInMultiplier * Time.deltaTime / _waitTime;
+            _audioSource.volume += _maxVolume * (Time.deltaTime / _fadeInTime);
             yield return null;
         }
 
@@ -147,7 +146,7 @@ public class WaterFallManager : MonoBehaviour, IGameEvent
         _audioSource.volume = _maxVolume;
         while (_audioSource.volume > 0.0f)
         {
-            _audioSource.volume -= _fadeOutMultiplier * Time.deltaTime / _waitTime;
+            _audioSource.volume -= _maxVolume * (Time.deltaTime / _fadeOutTime);
             yield return null;
         }
 
